45c0b4f9be9642d1275596344b96b6df6ee4cab7
happy_gagarara12 <46888825+happygagarara12@users.noreply.github.com>
2020-12-27 15:52:06 +0700

    Create Falcon SS Game Joiner.js

---
+++ Falcon SS Game Joiner.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Falcon SS Game Joiner
+// @namespace    name?
+// @version      0.1
+// @description  For Falcon SS Users
+// @author       unstable#8768
+// @match        https://web.roblox.com/games/*
+// @grant        none
+// ==/UserScript==
+
+function GetURLParameter(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split("&");
+    for (const index = 0; index < sURLVariables.length; index++){
+        var sParameterName = sURLVariables[index].split("=");
+        if (sParameterName[0] == sParam){
+            return sParameterName[1];
+        }
+    }
+}
+
+function main(){
+    var PlaceId = GetURLParameter("placeid");
+    var JobId = GetURLParameter("jobid");
+    if (PlaceId && JobId){
+        Roblox.GameLauncher.joinGameInstance(PlaceId, JobId);
+    }
+}
+
+document.body.onload = main();
